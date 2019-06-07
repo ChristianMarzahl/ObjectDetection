8e870224870d1cadaf81943e8c66ced86c7c4ce5
c.marzahl@de.eu.local <c.marzahl@euroimmun.de>
2019-06-07 09:20:14 +0200

    added option to return ID`s from nms

--- helper/nms_center_distance.py
+++ helper/nms_center_distance.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.neighbors import KDTree
 
 
-def non_max_suppression_by_distance(boxes, scores, radius: float = 25):
+def non_max_suppression_by_distance(boxes, scores, radius: float = 25, return_ids = False):
 
     center_x = boxes[:, 0] + (boxes[:, 2] - boxes[:, 0]) / 2
     center_y = boxes[:, 1] + (boxes[:, 3] - boxes[:, 1]) / 2
@@ -20,4 +20,4 @@ def non_max_suppression_by_distance(boxes, scores, radius: float = 25):
         ids_to_keep.append(id)
         sorted_ids = np.delete(sorted_ids, np.in1d(sorted_ids, ind[id]).nonzero()[0])
 
-    return boxes[ids_to_keep]
+    return boxes[ids_to_keep] if return_ids == False else ids_to_keep
