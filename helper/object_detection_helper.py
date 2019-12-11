916a68728eec794c9a24ee280072adaa76efd9db
Marc Aubreville <marc.aubreville@fau.de>
2019-12-11 15:20:46 +0100

    Fix for current pytorch version

--- helper/object_detection_helper.py
+++ helper/object_detection_helper.py
@@ -230,7 +230,7 @@ def process_output(clas_pred, bbox_pred, anchors, detect_thresh=0.25):
     clas_pred = torch.sigmoid(clas_pred)
 
     detect_mask = clas_pred.max(1)[0] > detect_thresh
-    if np.array(detect_mask).max() == 0:
+    if np.array(detect_mask.cpu()).max() == 0:
         return None, None, None
 
     bbox_pred, clas_pred = bbox_pred[detect_mask], clas_pred[detect_mask]
